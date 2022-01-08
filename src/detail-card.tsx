2295bbaeafdd33539cf2a67b6d4b300a0a48824c
captainSpades <ruimprego@gmail.com>
2022-01-08 00:32:11 +0000

    updates

--- src/detail-card.tsx
+++ src/detail-card.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './detail-card.css';
+import mapIcon from './assets/map.svg'
 
 function DetailCard(props : any) {
     return (
         <div key={props.id} className="card">
             <img className="detail-image" src={props.place.image} alt={props.place.text} />
-            <span className="card-title">{props.place.title}</span>
-            <span className="card-text">{props.place.text}</span>
-            <a  href={props.place.location} className="card-link">Direções</a>
+            <span className="card-cell">
+                <span className="card-title">{props.place.title}</span>
+                <span className="card-text">{props.place.text}</span>
+                <a  href={props.place.location} className="card-link"><img className="map-icon" src={mapIcon} />Direções</a>
+            </span>
         </div>
     )
 }
